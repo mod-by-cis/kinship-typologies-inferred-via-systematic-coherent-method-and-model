288711b0b3d03eb34d675a34012ae0ec7fd07150
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2025-06-04 21:47:29 +0200

    Update default.ts

--- docs/config/default.ts
+++ docs/config/default.ts
@@ -8,4 +8,36 @@ const defaultOrderingTable1 =
     "hA","hZ","hAZ","=||","=||",
     "kiA","kjA","=||","kiZ","kjZ"];
 
-export {defaultOrderingTable1}
+const defaultPropsValsTable1 = new Map<string, number>([
+            ["h", 1],
+            ["i", 1],
+            ["j", 1],
+            //" ",
+            ["hi", 1],
+            ["hj", 1],
+            //" ",
+            ["ki", 1],
+            ["mi", 1],
+            ["li", 1],
+            //" ",
+            ["lj", 1],
+            ["wj", 1],
+            ["kj", 1],
+            //" ",
+            ["hA", 1],
+            ["hZ", 1],
+            ["hAZ", 1],
+            //" ",
+            ["kiA", 1],
+            ["kjA", 1],
+            //" ",
+            ["kiZ", 1],
+            ["kjZ", 1],
+            //" ",
+            ["=||", 20],
+          ]);
+
+export {
+  defaultOrderingTable1,
+  defaultPropsValsTable1,
+};
