be8e548622b849b4500fd8c4b724dab6a81b3f31
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2025-06-04 21:40:41 +0200

    Update default.ts

--- docs/config/default.ts
+++ docs/config/default.ts
@@ -1,8 +1,11 @@
 //./docs/config/default.ts
 
 const defaultOrderingTable1 =
-   ["mi","li","ki","=||","wj","lj","kj","=||","=||","hi","h","hj",
-   "=||","=||","i","j","=||","=||","hA","hZ","hAZ","=||","=||",
-   "kiA","kjA","=||","kiZ","kjZ"];
+   ["mi","li","ki","=||",
+    "wj","lj","kj","=||","=||",
+    "hi","h","hj", "=||","=||",
+    "i","j","=||","=||",
+    "hA","hZ","hAZ","=||","=||",
+    "kiA","kjA","=||","kiZ","kjZ"];
 
 export {defaultOrderingTable1}
